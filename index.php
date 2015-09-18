14169d452381c05e1ce7becdde67203892a80ac7
matt <mfransen1997@gmail.com>
2015-09-18 11:06:41 -0500

    Removed breadcrum.

--- index.php
+++ index.php
@@ -228,11 +228,6 @@
           <h1>
             <?php echo get_object_vars($page)['content-title']?>
           </h1>
-          <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li><a href="#">Examples</a></li>
-            <li class="active">Blank page</li>
-          </ol>
         </section>
 
         <!-- Main content -->
