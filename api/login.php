a46ece12f42125b8d69550e432813be1e60cd155
matt <mfransen1997@gmail.com>
2015-09-23 09:38:22 -0500

    Modified api

---
+++ api/login.php
@@ -0,0 +1,13 @@
+<?php
+	$result = new stdClass();
+	require("../data/config.php");;
+	$database = new mysqli($config->ip,$config->username,$config->password,$config->database,$config->port);
+	
+	if($database->connect_error) {
+		$result->error = "Connection Error.";
+	}else {
+		$function = $_GET['function'];
+		
+	}
+	echo json_encode($result);
+?>
