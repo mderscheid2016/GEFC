0312dbbb55cd4274e52fa6807f8b3e50e3b29ae4
matt <mfransen1997@gmail.com>
2015-10-20 10:37:16 -0500

    idk

--- index.php
+++ index.php
@@ -87,16 +87,6 @@
       <aside class="main-sidebar">
         <!-- sidebar: style can be found in sidebar.less -->
         <section class="sidebar">
-          <!-- search form -->
-          <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-              <input type="text" name="q" class="form-control" placeholder="Search For Child...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
-              </span>
-            </div>
-          </form>
-          <!-- /.search form -->
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu">
             <li style="color: #fff; text-align: center" class="header">Selected Child: <br><b>Joe Dirt</b></li>
